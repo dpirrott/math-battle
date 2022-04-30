8651b17cc9e38e503c6d8d5dbd11cfb55b9f0be8
Dylan Pirrotta <dpirrotta9@hotmail.com>
2022-04-30 15:52:48 -0700

    Create form and print numbers in list when submitted via form in Quiz.jsx

--- client/src/components/Quiz.jsx
+++ client/src/components/Quiz.jsx
@@ -1,7 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Quiz = (props) => {
-  return <div>Quiz</div>;
+  const [list, setList] = useState([5, 6, 8]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const formatInput = e.target[0].value.trim();
+    if (formatInput === "") {
+      return;
+    }
+    const temp = [...list];
+    temp.push(formatInput);
+    e.target[0].value = "";
+    setList(temp);
+  };
+
+  const renderList = () => {
+    const testList = list.map((number, index) => {
+      console.log(`number: ${number}, index: ${index}`);
+      return <li key={index}>{number}</li>;
+    });
+    return testList;
+  };
+
+  return (
+    <div>
+      <h1>Quiz</h1>
+      <form onSubmit={(e) => handleSubmit(e)}>
+        <input type={"number"}></input>
+      </form>
+      <ul>{renderList()}</ul>
+    </div>
+  );
 };
 
 export default Quiz;
